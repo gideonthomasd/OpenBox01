2bfbea120d5116202c255364db4b0d02f2cf6c18
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-10-10 19:18:55 +0100

    Add files via upload

---
+++ getyay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syyu --noconfirm 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S pamac-aur
+yay -S ttf-zekton-rg
+yay -S obmenu-generator
+yay -S conky-lua-archers
